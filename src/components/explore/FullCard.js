b3b03a2a7f8e212ef938e0d2c89616cb1578ba06
RFields24 <omellyinc24@gmail.com>
2022-07-11 22:38:19 -0400

    started laying groudnwork for dynamic cards in explore section

--- src/components/explore/FullCard.js
+++ src/components/explore/FullCard.js
@@ -1,15 +1,69 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import podcastCover from '../img/pcard2.jpg'
 import exploreStyle from './explore.module.css'
 import {Link} from 'react-router-dom'
+import Shows from './fullcard.json'
 
 const epDescription = 'Not yet Fetched'
+const shows = Shows[0].podcast
+// class FullCard extends Component {
+
+
+
+//   constructor(){
+//     super();
+//     this.state = {
+//       shows: []
+//     }
+//   }
+
+//   componentDidMount(){
+//     fetch('/')
+//     .then(res => res.json())
+//     .then(podcast => this.setState({shows}, () => console.log('podcasts fetched..', podcast)))
+//   }
+
+//   render(){
+//     return(
+//       <Link key={podcast}to = "/podcasthome" className={exploreStyle.link}>
+//         {
+//           this.state.podcasts.map(podcast => {
+//             <img src={podcast.image} alt=""></img>
+//           })
+//         }
+//      </Link> 
+//     )
+//   }
+
+// }
+
 
 
 function FullCard() {
+
   return (
     <Link to = "/podcasthome" className={exploreStyle.link}>
-      <div className={exploreStyle.card}>
+      { 
+
+        // MAPPING PODCASTS TO EXPLORE SECTION
+        
+        shows.map((show, index) =>{
+          return(
+            <div className={exploreStyle.card}>
+              <div className={exploreStyle.imageContainer}>
+                <img className={exploreStyle.img} src={show.image} alt="" />
+              </div>
+              <div className={exploreStyle.info}>
+                <h2 className={exploreStyle.h2}>{show.title}</h2>
+                <p className={exploreStyle.p}>{show.description}</p>
+              </div>
+            </div>
+          )
+        })
+      }
+
+
+      {/* <div className={exploreStyle.card}>
         <div className={exploreStyle.imageContainer}>
           <img className={exploreStyle.img} src={podcastCover} alt="" />
         </div>
@@ -17,7 +71,7 @@ function FullCard() {
           <h2 className={exploreStyle.h2}>Best Of (NBA Draft preview: Could Paolo Banchero be the No. 1 pick?)</h2>
           <p className={exploreStyle.p}>{epDescription}</p>
         </div>
-      </div>
+      </div> */}
      </Link> 
   )
 }
--- src/components/row/PodCard.js
+++ src/components/row/PodCard.js
@@ -2,11 +2,24 @@ import React from 'react'
 import rowStyle from './row.module.css'
 import podcastCover from '../img/pcard2.jpg'
 import {Link} from 'react-router-dom'
+import Shows from '../explore/fullcard.json'
 
+const shows = Shows[0].podcast
 
 function PodCard() {
   return (
     <Link to = "/podcasthome" className={rowStyle.link}>
+
+      {/* {
+        shows.map((show, index) => {
+          return(
+            <div key={index} className={rowStyle.cardWrapper}>
+              <img className={rowStyle.cardPic} src={show.image} alt="Show"/>
+              <h3 className={rowStyle.h3}>{show.title}</h3>
+            </div>
+          )
+        })
+      } */}
       <div className={rowStyle.cardWrapper}>
         <img className={rowStyle.cardPic} src={podcastCover} alt="" />
         <h3 className={rowStyle.h3}>Skip and Shannon: Undisputed</h3>
